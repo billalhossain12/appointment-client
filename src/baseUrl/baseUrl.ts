60c1641a96b52b5c4359fb40cf1e48ba84f31972
billalhossain12 <billalhossain343434@gmail.com>
2026-01-28 09:49:36 +0600

    removed unnecessary folder

--- src/baseUrl/baseUrl.ts
+++
@@ -1,7 +0,0 @@
-export let baseUrl = "http://localhost:4000";
-
-if (import.meta.env.MODE === "development") {
-  baseUrl = "https://apiv2.k33Street.com";
-} else {
-  baseUrl = "http://localhost:4000";
-}
